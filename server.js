3ced9289598464c83b60ab5246835bacb7906772
RAPHTER1 <147110525+RAPHTER1@users.noreply.github.com>
2024-08-26 07:26:24 +0000

    UPDATE

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const { exec } = require('child_process');
 const express = require('express');
+const path = require('path');
+const statusRoutes = require('./routes/serverRoutes'); // Importer les routes
 const app = express();
 
-app.get('/server-status', (req, res) => {
-    exec('screen -list | grep minecraft-server', (error) => {
-        if (error) {
-            res.json({ running: false });
-        } else {
-            res.json({ running: true });
-        }
-    });
-});
+// Servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Utiliser les routes
+app.use('/', statusRoutes);
+
+// Écouter sur le port 3000
 app.listen(3000, () => {
-    console.log('Serveur backend en écoute sur le port 3000');
+    console.log('Serveur en écoute sur le port 3000');
 });
