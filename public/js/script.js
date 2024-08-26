971193f7d53e55aa65f4ddbdba22a4feb5756113
RAPHTER1 <147110525+RAPHTER1@users.noreply.github.com>
2024-08-26 08:36:37 +0000

    UPDATE

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,21 @@
-// Vérifier le statut du serveur
 fetch('/server-status')
     .then(response => response.json())
     .then(data => {
+        const startBtn = document.getElementById('start-btn');
         const statusText = document.getElementById('server-status');
+
         if (data.running) {
+            // Le serveur est en ligne : désactiver et changer la couleur
             statusText.textContent = 'Statut du serveur : En ligne';
             statusText.style.color = 'green';
+            startBtn.classList.remove('server-offline');
+            startBtn.classList.add('server-online');
         } else {
+            // Le serveur est hors ligne : activer et changer la couleur
             statusText.textContent = 'Statut du serveur : Hors ligne';
             statusText.style.color = 'red';
+            startBtn.classList.remove('server-online');
+            startBtn.classList.add('server-offline');
         }
     })
     .catch(err => {
